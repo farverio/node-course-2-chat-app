db0eed76dd3a751f6a6cfa3623455acd16847b1d
Stephen <stephen@farver.io>
2018-04-02 18:19:13 -0400

    create generateMessage utility

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,9 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('New message', message);
-})
+});
+
+socket.emit('createMessage', {
+  from: 'Frank',
+  text: 'hi'
+});
