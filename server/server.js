239c1b08c4d6a914ef54fbbb9118adc2f8a6b583
Stephen <stephen@farver.io>
2018-04-02 16:21:01 -0400

    added socket connections to server and client

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 const app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => console.log(`Server is up on ${port}!`))
+io.on('connection', (socket) => {
+  console.log('new user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+
+server.listen(port, () => console.log(`Server is up on ${port}!`))
